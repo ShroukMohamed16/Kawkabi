db97bf1936761b6198152dd4835c96b7a66ebb6f
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2026-02-03 18:29:36 +0200

    refactor `ChildAddedScreen` image modifiers and add Arabic string resources

--- app/src/main/java/com/example/kawkabi/ui/presentation/assessment/ChildAddedScreen.kt
+++ app/src/main/java/com/example/kawkabi/ui/presentation/assessment/ChildAddedScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -55,15 +56,13 @@ fun ChildAddedContent(
                 .aspectRatio(1f)
 
         )
-
         Spacer(Modifier.height(32.dp))
 
         Image(
             painter = painterResource(R.drawable.ic_check),
             contentDescription = null,
             modifier = Modifier
-                .width(80.dp)
-                .height(80.dp)
+                .size(80.dp)
 
         )
         Spacer(Modifier.height(24.dp))
